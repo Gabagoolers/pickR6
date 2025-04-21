dc8c35dabe7a48591442de93736c60d011504af4
Miklos Daniel <daniel.miklos0@gmail.com>
2025-04-21 03:11:50 +0200

    chore: fix prettier warnings

--- packages/pickr6-svelte/src/routes/+page.svelte
+++ packages/pickr6-svelte/src/routes/+page.svelte
@@ -69,7 +69,7 @@
 					</div>
 					<div class="flex items-center justify-between space-x-2">
 						<Label for="hide-starter"
-							>Hide starter operators <span class="text-muted-foreground truncate text-xs">
+							>Hide starter operators <span class="truncate text-xs text-muted-foreground">
 								Recruit, Striker, Sentry
 							</span></Label
 						>
@@ -110,7 +110,7 @@
 				</h2>
 				<p>
 					Head over to the <a
-						class="text-primary font-medium underline underline-offset-4"
+						class="font-medium text-primary underline underline-offset-4"
 						href="/settings/owned-operators">Owned Operators</a
 					> page to set which operators you own.
 				</p>
