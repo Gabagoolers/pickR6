89b1080e70b863384a71b353c93017e56e72f182
Miklos Daniel <daniel.miklos0@gmail.com>
2025-04-21 03:18:02 +0200

    fix: for on label

--- packages/pickr6-svelte/src/lib/components/ui/dropdown-menu/dropdown-menu-label.svelte
+++ packages/pickr6-svelte/src/lib/components/ui/dropdown-menu/dropdown-menu-label.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { cn } from "$lib/utils.js";
-	import { type WithElementRef } from "bits-ui";
-	import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from '$lib/utils.js';
+	import { type WithElementRef } from 'bits-ui';
+	import type { HTMLAttributes } from 'svelte/elements';
 
 	let {
 		ref = $bindable(null),
@@ -11,12 +11,13 @@
 		...restProps
 	}: WithElementRef<HTMLAttributes<HTMLDivElement>> & {
 		inset?: boolean;
+		for?: string;
 	} = $props();
 </script>
 
 <div
 	bind:this={ref}
-	class={cn("px-2 py-1.5 text-sm font-semibold", inset && "pl-8", className)}
+	class={cn('px-2 py-1.5 text-sm font-semibold', inset && 'pl-8', className)}
 	{...restProps}
 >
 	{@render children?.()}
