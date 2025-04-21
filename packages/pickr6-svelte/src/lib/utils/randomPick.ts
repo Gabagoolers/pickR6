dbd0d486f44abb2c1535f914bd4e2036a246e0ff
Miklos Daniel <daniel.miklos0@gmail.com>
2025-04-21 20:46:26 +0200

    fix: filtering by side and hideStarters

---
+++ packages/pickr6-svelte/src/lib/utils/randomPick.ts
@@ -0,0 +1,29 @@
+import type { OperatorSide } from '$lib/stores/persisted.svelte';
+import type { ReducedSanitizedOperator } from './operators';
+
+type PickableOperatorPoolOptions = {
+	hideStarter: boolean;
+	customSet?: Array<string>;
+};
+
+const starterOperators = [
+	'striker',
+	'sentry',
+	...['blue', 'green', 'orage', 'red', 'yellow'].map((e) => `recruit_${e}`)
+];
+
+const isStarterOpFunc = (x: ReducedSanitizedOperator) => starterOperators.includes(x.id);
+
+export function pickableOperatorPool<T extends ReducedSanitizedOperator>(
+	operators: Array<T>,
+	side: OperatorSide,
+	options?: PickableOperatorPoolOptions
+): Array<T> {
+	return operators
+		.filter((e) => {
+			const isStarter = isStarterOpFunc(e);
+			return (options?.hideStarter ?? true) ? !isStarter : true;
+		})
+		.filter((e) => e.role.toLowerCase() === side)
+		.filter((e) => (options?.customSet ? options.customSet.includes(e.id) : true));
+}
