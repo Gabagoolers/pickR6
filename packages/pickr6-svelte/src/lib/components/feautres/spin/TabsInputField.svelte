3db471491da1d72b833f6d6fb353e10adda5cb9e
Miklos Daniel <daniel.miklos0@gmail.com>
2025-04-27 11:44:28 +0200

    format

--- packages/pickr6-svelte/src/lib/components/feautres/spin/TabsInputField.svelte
+++ packages/pickr6-svelte/src/lib/components/feautres/spin/TabsInputField.svelte
@@ -11,14 +11,14 @@
 </script>
 
 <div
-	class="bg-muted text-muted-foreground inline-flex h-10 items-center justify-center rounded-md p-1"
+	class="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground"
 >
 	{#each options as o}
 		<button
 			type="button"
 			onclick={() => tabOnClick(o)}
 			data-state={$side === o ? 'active' : undefined}
-			class="ring-offset-background focus-visible:ring-ring data-[state=active]:bg-background data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm"
+			class="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
 		>
 			{o}
 		</button>
