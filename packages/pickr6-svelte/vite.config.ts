6aa0e92e646a4a1dbba62d952e6c586a0be84b3f
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2025-04-27 12:50:33 +0200

    test: unit tests for filtering logic (#40)

--- packages/pickr6-svelte/vite.config.ts
+++ packages/pickr6-svelte/vite.config.ts
@@ -1,6 +1,6 @@
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 
@@ -10,7 +10,30 @@ const pkg = JSON.parse(json);
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		PKG: pkg
+	define: { PKG: pkg },
+	test: {
+		workspace: [
+			{
+				extends: './vite.config.ts',
+				plugins: [svelteTesting()],
+				test: {
+					name: 'client',
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
 	}
 });
