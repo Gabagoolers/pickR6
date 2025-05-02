5c198bbc0a20a1a28a2b672567cea7f41596484e
Miklos Daniel <daniel.miklos0@gmail.com>
2025-05-02 19:10:58 +0200

    feat: satori preview img gen

---
+++ packages/pickr6-svelte/src/lib/components/feautres/seo/PreviewImage.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	const version = PKG.version;
+</script>
+
+<div
+	style="
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-image: linear-gradient(to bottom, #dbf4ff, #fff1f1);
+  font-size: 60px;
+  letter-spacing: -2px;
+  font-weight: 700;
+  text-align: center;
+"
+>
+	<div
+		style="
+    background-image: linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216));
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+  "
+	>
+		PickR6
+	</div>
+	<div
+		style="
+    background-image: linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128));
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+  "
+	>
+		{version}
+	</div>
+</div>
---
+++ packages/pickr6-svelte/src/routes/og-image.png/+server.ts
@@ -0,0 +1,16 @@
+export const prerender = dev ? false : true;
+
+import { dev } from '$app/environment';
+import PreviewImage from '$lib/components/feautres/seo/PreviewImage.svelte';
+import { componentToPng } from '$lib/server/renderImage';
+
+export const GET = async () => {
+	const width = 1200;
+	const height = 600;
+	return componentToPng(
+		PreviewImage,
+		{ seed: 'prerendered image', width, height, satori: true },
+		height,
+		width
+	);
+};
